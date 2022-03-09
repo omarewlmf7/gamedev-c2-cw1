cb4f583e36007d32a7bd45956bed6a7100ca1ff3
omarewlmf7 <omaar.30555@gmail.com>
2022-03-09 16:47:37 +0300

    c2-cw1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace c2_cw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "fawaz";
+            int age = 17;
+            int speed = 89;
+            string SuperPower1 = "invisible";
+            Console.WriteLine("I am " + characterName + " and my age is " + age + " my speed is " + speed + " my Super power is " + SuperPower1);
+        }
+    }
+}
